1c892436317c516d101e6e96354b5e90118935df
gabrys zych <gabrielzych.13@gmail.com>
2025-07-19 05:24:08 +0200

    Removed hello world 🫩

--- CatFacts/src/Program.cs
+++ CatFacts/src/Program.cs
@@ -4,9 +4,6 @@ using System.Runtime.CompilerServices;
 using CatFacts.src;
 
 
-
-Console.WriteLine("Hello, World! cat\u201ds //word//");
-
 using HttpClient client = new();
 var factFetcher = new FactFetcherService(client);
 
